b9ebaf35efd11fd6b4315a06b6901746247a9fd7
maxime <75097528+maximedondon@users.noreply.github.com>
2025-08-25 20:19:10 +0200

    Initial Commit

---
+++ projectmanager/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  projectmanager
+//
+//  Created by Maxime Dondon on 25/08/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ projectmanager/projectmanagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  projectmanagerApp.swift
+//  projectmanager
+//
+//  Created by Maxime Dondon on 25/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct projectmanagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
